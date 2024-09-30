bf01fb28055107a59a43e5caef3b7ec77b289fc9
Roan81 <robert.boepple@gmail.com>
2024-09-30 19:33:24 +0200

    update
    body and navbar

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import {AboveTheFoldComponent} from './above-the-fold/above-the-fold.component';
-import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, AboveTheFoldComponent, FooterComponent, AboutmeComponent],
+  imports: [AboveTheFoldComponent, FooterComponent, AboutmeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
