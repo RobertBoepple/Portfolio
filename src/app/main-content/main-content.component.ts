633e2e278252d8378231d0ef215909855115024e
Roan81 <robert.boepple@gmail.com>
2024-09-30 15:18:13 +0200

    update
    base structure portfolio

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {AboveTheFoldComponent} from './above-the-fold/above-the-fold.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, AboveTheFoldComponent, FooterComponent, AboutmeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
