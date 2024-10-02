e551dd5aa347fc07c5d93a9b1069c1c0eeaf6233
RobertBoepple <robert.boepple@gmail.com>
2024-10-02 23:02:10 +0200

    Revert "add npm Translate + update language button"
    This reverts commit b1318dbcc572a7d0d298a9e66a814f654909ce6b.

--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -1,26 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
-  isGerman: boolean = false;
-  
-  constructor(public translate: TranslateModule) {
 
-  }
-  toggleLanguage() {
-    if (this.isGerman) {
-      this.isGerman = false;
-    } else {
-      this.isGerman = true;
-    }
-    console.log('Deutsch ist an: ' + this.isGerman);
-  }
 }
