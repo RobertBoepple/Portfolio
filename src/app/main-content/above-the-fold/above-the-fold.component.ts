65722b91fd4d59112f5b6ae983ffa8cdb20c2405
RobertBoepple <robert.boepple@gmail.com>
2024-10-03 13:12:48 +0200

    add translation to above the fold

--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../translation.service';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
+  translate = inject(TranslationService);
 
+  constructor(private translationService: TranslationService) {}
+
+  toggleLanguage(event: any): void {
+    const language = event.target.checked ? 'de' : 'en';
+    this.translationService.switchLanguage(language);
+  }
 }
+
