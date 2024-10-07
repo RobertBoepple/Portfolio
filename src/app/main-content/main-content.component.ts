0bef27c6140bdd4947e18c76fdd3d231a8fb9b80
RobertBoepple <robert.boepple@gmail.com>
2024-10-07 20:46:42 +0200

    add myskill component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {AboveTheFoldComponent} from './above-the-fold/above-the-fold.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
+import { MyskillsComponent } from './myskills/myskills.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [AboveTheFoldComponent, FooterComponent, AboutmeComponent],
+  imports: [AboveTheFoldComponent, FooterComponent, AboutmeComponent, MyskillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
