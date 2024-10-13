c119615f0c03b6335ff39b5704c6c9f78fb4c671
RobertBoepple <robert.boepple@gmail.com>
2024-10-14 00:24:16 +0200

    add form to contact component

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    privacy: false,
+  };
 }
