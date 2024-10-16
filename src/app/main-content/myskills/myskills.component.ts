2cfa17abb1b3dc87417910d77191797a5622fc4f
RobertBoepple <robert.boepple@gmail.com>
2024-10-16 22:20:34 +0200

    add popup Skillset

--- src/app/main-content/myskills/myskills.component.ts
+++ src/app/main-content/myskills/myskills.component.ts
@@ -1,14 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../../translation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-myskills',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './myskills.component.html',
   styleUrl: './myskills.component.scss'
 })
 export class MyskillsComponent {
+  mind: boolean = false;
+
   translate = inject(TranslationService);
+
+  hover(movement: string) {
+    if (movement == 'in') {
+      this.mind = true;
+      console.log('hovered in');
+    } else {
+      this.mind = false;
+    }
+  }
 }
