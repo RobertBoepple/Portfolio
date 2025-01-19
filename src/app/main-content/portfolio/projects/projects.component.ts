b4fdc69ebcb3ed1bca6d21c12fa1d6d2a7b869a4
Robert Böpple <110339856+RobertBoepple@users.noreply.github.com>
2025-01-19 23:48:02 +0100

    add overlay to projects

--- src/app/main-content/portfolio/projects/projects.component.ts
+++ src/app/main-content/portfolio/projects/projects.component.ts
@@ -1,56 +1,52 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+interface Project {
+  name: string;
+  techStack: { name: string }[];
+  description: string;
+  githubLink: string;
+  liveLink: string;
+  imgPath: string;
+  index: number;
+}
+
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
   showArrowHover: { [key: number]: boolean } = {};
-  imgPathProject: string='';
+  imgPathProject: string = '';
+  selectedProject: Project | null = null;
 
-  onMouseEnter(index: number) {
-    this.showArrowHover[index] = true;
-    this.hoverProject(index);
-  }
-  
-  onMouseLeave(index: number) {
-    this.showArrowHover[index] = false;
-    this.imgPathProject = '';
-  }
-
-  hoverProject(index: number) {
-    if (index === 0) {
-      this.imgPathProject = 'assets/img/join.jpg';
-    } else if (index === 1) {
-      this.imgPathProject = 'assets/img/El Pollo Loco.png';
-    } else if (index === 2) {
-      this.imgPathProject = 'assets/img/Pokedex.png';
-    }
-  }
-
-  projects = [
+  projects: Project[] = [
     {
       name: 'Join',
       techStack: [
         { name: 'JavaScript' },
         { name: 'HTML' },
         { name: 'CSS' },
-        { name: 'Firebase' }
+        { name: 'Firebase' },
       ],
-      index: 0
+      description:
+        'Ein Kanban-Board Projekt für Aufgabenverwaltung und Teamkollaboration.',
+      githubLink: 'https://github.com/IhrGithubLink/Join',
+      liveLink: 'https://IhreLiveDemoURL/Join',
+      imgPath: 'assets/img/join.jpg',
+      index: 0,
     },
     {
       name: 'El Pollo Loco',
-      techStack: [
-        { name: 'JavaScript' },
-        { name: 'HTML' },
-        { name: 'CSS' }
-      ],
-      index: 1
+      techStack: [{ name: 'JavaScript' }, { name: 'HTML' }, { name: 'CSS' }],
+      description: 'Ein JavaScript-basiertes Jump-and-Run Spiel.',
+      githubLink: 'https://github.com/IhrGithubLink/ElPolloLoco',
+      liveLink: 'https://IhreLiveDemoURL/ElPolloLoco',
+      imgPath: 'assets/img/El Pollo Loco.png',
+      index: 1,
     },
     {
       name: 'Pokédex',
@@ -58,11 +54,45 @@ export class ProjectsComponent {
         { name: 'JavaScript' },
         { name: 'HTML' },
         { name: 'CSS' },
-        { name: 'Rest-Api' }
+        { name: 'Rest-Api' },
       ],
-      index: 2
-    }
+      description: 'Eine Pokémon-Enzyklopädie mit Daten aus der PokéAPI.',
+      githubLink: 'https://github.com/IhrGithubLink/Pokedex',
+      liveLink: 'https://IhreLiveDemoURL/Pokedex',
+      imgPath: 'assets/img/Pokedex.png',
+      index: 2,
+    },
   ];
-  
-}
 
+  onMouseEnter(index: number) {
+    this.showArrowHover[index] = true;
+    this.hoverProject(index);
+  }
+
+  onMouseLeave(index: number) {
+    this.showArrowHover[index] = false;
+    this.imgPathProject = '';
+  }
+
+  hoverProject(index: number) {
+    console.log('Hover Index:', index);
+    const project = this.projects[index];
+    if (project) {
+      console.log('Project found:', project);
+      console.log('Image path:', project.imgPath);
+      this.imgPathProject = project.imgPath;
+    }
+  }
+
+  openProjectDetails(project: Project) {
+    console.log('Opening project:', project);
+    this.selectedProject = project;
+    this.imgPathProject = project.imgPath;
+    console.log('Set image path to:', this.imgPathProject);
+  }
+
+  closeOverlay() {
+    this.selectedProject = null;
+    this.imgPathProject = '';
+  }
+}
