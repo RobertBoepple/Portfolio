4b35a1b0a8077d380f39b13f3b6a452d94bc053f
Robert <robert.boepple@gmail.com>
2025-03-30 15:22:44 +0200

    update reference component

--- src/app/main-content/references/references.component.ts
+++ src/app/main-content/references/references.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { QuotesComponent } from './quotes/quotes.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../translation.service';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [QuotesComponent],
+  imports: [QuotesComponent, TranslateModule],
   templateUrl: './references.component.html',
   styleUrl: './references.component.scss'
 })
 export class ReferencesComponent {
-
+  translate = inject(TranslationService);
 }
