9730a13cee7adee6074e30fd6a6f8314b76f9325
Robert <robert.boepple@gmail.com>
2025-04-07 21:43:21 +0200

    update imprint translation

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { FooterComponent } from '../shared/components/footer/footer.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../translation.service';
+
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
+translate = inject(TranslationService);
 }
